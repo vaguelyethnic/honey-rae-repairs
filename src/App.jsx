599d49b6d733191976bddb131eaa7a38c5b09ff2
Isabel Aparicio <iaparicio695@gmail.com>
2024-07-12 08:30:23 -0700

    files tracked

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import { getAllTickets } from "./services/ticketService";
+import "src/App.css";
+
+export const App = () => {
+  const [allTickets, setAllTickets] = useState([])
+
+  useEffect(() => {
+    getAllTickets().then((ticketArray) => {
+      setAllTickets(ticketArray)
+      console.log("tickets set")
+    })
+  }, [])
+    
+
+
+ 
+
+  return( 
+    <div className="tickets-container">
+      <h2>Tickets</h2>
+      <article className="tickets">
+        {allTickets.map(ticket => {
+          return (
+             <section className="ticket" key={ticket.id}>
+              <header className="ticket-info">#{ticket.id}</header>
+              <div>{ticket.description}</div>
+              <footer>
+                <div className="ticket-info">Emergency</div>
+                <div>{ticket.emergency ? "yes" : "no"}</div>
+              </footer>
+             </section>
+          )
+        })}
+      </article>
+    </div>)
+}
